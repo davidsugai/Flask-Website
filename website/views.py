c45d1b6a3983bba82355bb568ccc927af67e9769
David Sugai <sugaidav@msu.edu>
2023-01-21 14:34:34 -0500

    Navbar added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>First Page!<h1>"
+    return render_template("home.html")
+
+@views.route('/contact')
+def contact():
+    return render_template("contact.html")
